37b91aaaf9edc514475394fc45a831d52e843871
RENEGAD0 <raul.parra@estudiantat.upc.edu>
2022-12-02 10:49:38 +0100

    Creation of zone 2

--- Dungeons Heroes/Assets/Scripts/Player/Controller.cs
+++ Dungeons Heroes/Assets/Scripts/Player/Controller.cs
@@ -14,13 +14,13 @@ public class Controller : MonoBehaviour
     void Update()
     {
         if (Input.GetKey(KeyCode.W))
-            gameObject.transform.Translate(speed * Time.deltaTime, 0.0f, 0.0f, Space.World);
-        if (Input.GetKey(KeyCode.S))
-            gameObject.transform.Translate(-speed * Time.deltaTime, 0.0f, 0.0f, Space.World);
-        if (Input.GetKey(KeyCode.A))
-            gameObject.transform.Translate(0.0f, 0.0f, speed * Time.deltaTime, Space.World);
-        if (Input.GetKey(KeyCode.D))
             gameObject.transform.Translate(0.0f, 0.0f, -speed * Time.deltaTime, Space.World);
+        if (Input.GetKey(KeyCode.S))
+            gameObject.transform.Translate(0.0f, 0.0f, speed * Time.deltaTime, Space.World);
+        if (Input.GetKey(KeyCode.A))
+            gameObject.transform.Translate(speed * Time.deltaTime, 0.0f, 0.0f, Space.World);
+        if (Input.GetKey(KeyCode.D))
+            gameObject.transform.Translate(-speed * Time.deltaTime, 0.0f, 0.0f, Space.World);
         
     }
 
